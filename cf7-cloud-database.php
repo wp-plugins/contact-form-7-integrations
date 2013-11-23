4818639d72be58b1550852368562ae397091a19a
contactus.com <contactus.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-23 01:09:04 +0000

    loader
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-integrations/trunk@809119 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cf7-cloud-database.php
+++ cf7-cloud-database.php
@@ -5,8 +5,8 @@
 * Description:Database, analytics and software integrations for Contact Form 7
 * Author: http://www.contactus.com
 * Author URI: http://www.contactus.com/
-* Version: 1.0 Beta
-* Stable tag: 1.0 BETA
+* Version: 1.0.1 Beta
+* Stable tag: 1.0.1 BETA
 * License: GPLv2 or later
 **/
 
